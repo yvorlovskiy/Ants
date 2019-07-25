e075adc2667cc3b418ac9c5f0c4365648fe16708
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2019-07-24 17:00:47 -0700

    added seed capability

--- Data-Collection/TrainTest.py
+++ Data-Collection/TrainTest.py
@@ -1,4 +1,4 @@
-import glob, os
+import glob, os, random
 # Current directory
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -6,7 +6,6 @@ print(current_dir)
 current_dir = 'antdata'
 
 # Percentage of images to be used for the test set
-
 percentage_test = 10
 
 # Create and/or truncate train.txt and test.txt
@@ -24,3 +23,24 @@ for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.txt")):
     else:
         file_train.write(current_dir + "/" + title + '.jpg' + "\n")
         counter = counter + 1
+        
+"""
+# This may also work
+train_image_amount = round((1-percentage_test)*len(glob.glob(os.path.join(current_dir, "*.txt")))/100)
+images = glob.glob(os.path.join(current_dir, "*.txt"))
+
+random.seed(4)
+random.shuffle(images)
+
+train_images = images[0:train_image_amount]
+test_images = images[:train_image_amount]
+
+def image_write(images, file):
+    for image in images:
+        title, _ = os.path.splitext(os.path.basename(image))
+        file.write(current_dir + "/" + title + '.jpg' + "\n")
+        
+image_write(train_images, file_train)
+image_write(test_images, file_test)
+    
+"""
